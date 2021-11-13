044b194f2e98898b7d7ae992cba3c47e05a579c2
jaswanth kumar <jaswanthkumark100@gmail.com>
2021-11-13 18:33:36 +0530

    Initial commit

--- src/com/geeksForgeeeks/AnalysisOfAlgorithm/SumOfNaturalNumbers.java
+++ src/com/geeksForgeeeks/AnalysisOfAlgorithm/SumOfNaturalNumbers.java
@@ -2,6 +2,9 @@ package com.geeksForgeeeks.AnalysisOfAlgorithm;
 
 public class SumOfNaturalNumbers {
 
+	
+	/*** Analysis Of Algorith O(1) ***/
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	int n=10;
